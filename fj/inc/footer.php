ee8dfc2671acd22858caf44abc7ec5462f9ac713
Def-Terashita <y-terashita@e-deftribe.com>
2020-04-17 15:42:53 +0900

    【共通】ページのトップの位置が競合している分を戻しました。フッターいらん遷移先削除。SNSアイコン追加

--- fj/inc/footer.php
+++ fj/inc/footer.php
@@ -45,7 +45,6 @@ $(document).ready(function() {
     		<div class="footer-contents">
     			<ul>
     				<li><a href="<?= Fj_CorporateInfo ?>">会社概要</a></li>
-    				<li><a href="<?= Fj_CorporateInfo ?>#map">アクセス</a></li>
     				<li><a href="<?= Fj_PrivacyPolicy ?>">プライバシーポリシ</a></li>
     				<li><a href="<?= Fj_SecurityPolicy ?>">情報セキュリティポリシ</a></li>
     			</ul>
@@ -55,16 +54,7 @@ $(document).ready(function() {
     			<ul>
                     <li><a href="<?= Fj_Staff ?>">コーディネーター</a></li>
     				<li><a href="<?= Fj_Service ?>">サービス紹介</a></li>
-    				<li><a href="<?= Fj_FAQ ?>">FAQ</a></li>
     				<li><a href="<?= Fj_Voice ?>">口コミ</a></li>
-    			</ul>
-    		</div>
-    		<div class="sp-space"></div>
-    		<div class="footer-contents">
-    			<ul>
-    				<li><a href="<?= Fj_Top ?>#searchbox">仕事を探す</a></li>
-    				<li><a href="<?= Fj_Form ?>">お問い合わせ</a></li>
-    				<?= $login ?>
     			</ul>
     		</div>
     		<div class="clear sp-space"></div>
@@ -72,15 +62,18 @@ $(document).ready(function() {
 
     	<div class="footer-colm2">
     		<div id="adress">
-    			<ul>
+          <ul>
+            <li id="footer-snsimg"<a href="https://twitter.com/def_tribe"><img alt="Twitterアイコン" src="<?= Fj_Img ?>Twitterアイコン.png"></a>
+            <li id="footer-snsimg"<a href="https://www.instagram.com/def_tribe/?hl=ja"><img alt="インスタアイコン" src="<?= Fj_Img ?>インスタアイコン.png"></a>
+            <li id="footer-snsimg"<a href="https://ja-jp.facebook.com/pages/category/Internet-Company/Def-tribe-group%E6%A0%AA%E5%BC%8F%E4%BC%9A%E7%A4%BEDef-tribe-195375853971980/"><img alt="Facebookアイコン" src="<?= Fj_Img ?>Facebookアイコン.png"></a>
     				<li id="footer-img"><a href="<?= Fj_Top ?>"><img alt="Bootanのロゴ" src="<?= Fj_Img ?>Bootan.png"></a></li>
     				<li>大阪市中央区南船場4丁目12番24号 現代心斎橋ビル5階</li>
     				<li><a href="tel:0666439305">TEL 06-6643-9305</a></li>
     			</ul>
     		</div>
     	</div>
+
     	<div class="clear"></div>
 	</div>
 
 </footer>
-
